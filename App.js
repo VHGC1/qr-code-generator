f80c13a98eb2fd111e37e96095a6199741298cde
vitor costa <vitorjjhg@hotmail.com>
2021-09-02 12:56:17 -0300

    innitial-commit

---
+++ App.js
@@ -0,0 +1,27 @@
+let qrcode = new QRCode("qrcode", {
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H,
+});
+
+let text = document.getElementById("text");
+let button = document.getElementById("button");
+let downloadButton = document.getElementById("downloadButton")
+
+qrcode.makeCode(window.location);
+
+button.onclick = () => {
+  qrcode.makeCode(text.value);
+};
+
+downloadButton.onclick = () => {
+  download()
+}
+
+var download = function(){
+  var link = document.createElement('a');
+  link.download = 'QrCode.png';
+  link.href = document.getElementsByTagName('canvas')[0].toDataURL()
+
+  link.click();
+}
